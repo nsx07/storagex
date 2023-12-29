ee5a7a13e1d812a28b221b9ee7600aa651d030d1
nsx07 <felipeaugustodluz@hotmail.com>
2023-12-29 17:05:18 -0300

    add open-replay

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import Tracker from '@openreplay/tracker';
 
 bootstrapApplication(AppComponent, appConfig)
+  .then((ref) => new Tracker({ projectKey: "LZtkmHfnErC4rxd5KFq1" }).start())
   .catch((err) => console.error(err));
