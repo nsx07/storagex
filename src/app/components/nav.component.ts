68bc0251e93a1dc7da6cfe0170524a26b5c50d27
Felipe Pires <97983021+nsx07@users.noreply.github.com>
2023-12-29 15:49:45 -0300

    Update nav.component.ts

--- src/app/components/nav.component.ts
+++ src/app/components/nav.component.ts
@@ -25,7 +25,7 @@ import { ContextItem, ContextMenuComponent } from "./context-menu.component";
 
             
             <div class="flex md:order-2">
-                <button class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
+                <button class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
                     id="theme-toggle" type="button">
                     <svg id="theme-toggle-dark-icon" class="w-5 h-5 hidden" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
@@ -48,7 +48,7 @@ import { ContextItem, ContextMenuComponent } from "./context-menu.component";
                                 </span>
                             </span>
                         } @else {
-                            <span class="p-2 rounded-md border-2 shadow flex gap-2" inert>
+                            <span class="p-2 rounded-md border-2 flex gap-2" inert>
                                   <span>Acess</span>
                                   <span>
                                       <i class="fa-solid fa-right-from-bracket"></i>
